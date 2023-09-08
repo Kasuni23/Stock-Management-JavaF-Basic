985641fb019f98d7a66c34ac6a8713125a4966f4
kasuni23 <kasunimaheshika2325@gmail.com>
2023-09-09 02:03:27 +0530

    complete login form

---
+++ src/main/java/lk/ijse/stockmanagement/controller/LoginFormController.java
@@ -0,0 +1,44 @@
+package lk.ijse.stockmanagement.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.stockmanagement.db.db;
+
+import java.io.IOException;
+
+public class LoginFormController {
+    public TextField txtpw;
+    public TextField txtuser;
+    public AnchorPane root;
+
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+
+        String user = txtuser.getText();
+        String pw = txtpw.getText();
+        if (user.equals(db.user) && pw.equals(db.pw)){
+            //dash board --> go
+            navigate();
+        }else {
+            new Alert(Alert.AlertType.ERROR,"Invalid Login.Try Again").show();
+        }
+    }
+
+    public void navigate() throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/dashboard_form.fxml"));
+
+        Scene scene = new Scene(root);
+        Stage stage= (Stage) this.root.getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.setTitle("DashBoard");
+        stage.centerOnScreen();
+
+
+    }
+}
---
+++ src/main/java/lk/ijse/stockmanagement/db/db.java
@@ -0,0 +1,8 @@
+package lk.ijse.stockmanagement.db;
+
+public class db {
+
+    public static final String user = "Kasuni";
+    public static final String pw = "123456";
+
+}
