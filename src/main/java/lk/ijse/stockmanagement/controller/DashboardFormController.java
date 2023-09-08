9f4be6eab983be6cf68b12bb2f2f0e95fcd90208
kasuni23 <kasunimaheshika2325@gmail.com>
2023-09-09 01:01:37 +0530

    add customer icon

---
+++ src/main/java/lk/ijse/stockmanagement/controller/DashboardFormController.java
@@ -0,0 +1,57 @@
+package lk.ijse.stockmanagement.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashboardFormController {
+    public AnchorPane rootNode;
+    public AnchorPane node;
+
+    public void btnCustomerOnAction(ActionEvent actionEvent) throws IOException {
+        //open the customer manage form
+
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/customer_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+
+        stage.setTitle("Customer Manage");
+        stage.show();
+
+
+    }
+
+    public void btnItemOnAction(ActionEvent actionEvent) throws IOException {
+        //open item manage form to the dashboard stage
+        // dn tiyena window ekatama load krnna
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/item_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        
+        Stage primaryStage = (Stage) this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Item Manage");
+        //primaryStage.show();
+
+
+
+    }
+
+    public void btnSupplierOnAction(ActionEvent actionEvent) throws IOException {
+        //
+       Parent root = FXMLLoader.load(this.getClass().getResource("/view/supplier_form.fxml"));
+
+      // Scene scene = new Scene(root);
+       this.node.getChildren().clear();
+       this.node.getChildren().add(root);
+    }
+}
